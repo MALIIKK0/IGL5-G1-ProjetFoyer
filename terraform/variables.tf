96f93d4465767b4b635056bd8f322b4c05533cbe
MALIIKK0 <123571028+MALIIKK0@users.noreply.github.com>
2024-11-07 09:04:58 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,35 @@
 variable "aws_region" {
   description = "La région AWS"
   type        = string
-  default     = "us-east-1" # Région mise à jour
+  default     = "us-east-1"  # Région mise à jour
 }
 
 variable "cluster_name" {
   description = "Nom du cluster EKS"
   type        = string
-  default     = "KubeCluster" # Nom du cluster mis à jour
+  default     = "mykubernetes"  # Nom du cluster mis à jour
 }
 
+variable "subnet_ids" {
+  description = "IDs des sous-réseaux"
+  type        = list(string)
+  default     = ["subnet-013500d765bf66d35", "subnet-05458a0c56bdf16a5"]  # Valeurs par défaut
+}
+
+variable "role_arn" {
+  description = "ARN du rôle IAM pour EKS"
+  type        = string
+  default     = "arn:aws:iam::744983671605:role/eksRole"  # Valeur par défaut
+}
+
+variable "vpc_id" {
+  description = "L'ID du VPC pour le cluster EKS"
+  type        = string
+  default     = "vpc-01fa33a7e0f700fdd"  # Remplacez par votre ID de VPC réel
+}
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16" # Modifiez-le selon vos besoins
+  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
 }
